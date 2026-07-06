b86ea23a6a44e90f954050e383417cdfa8b62c0a
nomiraclesthere <nomiraclesthere@users.noreply.github.com>
2026-07-06 10:25:58 +0000

    deploy: 93eac368410dfa93e3f7137b945c441a9abb2210

---
+++ assets/trailer-BwijBPYk.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vendor-vue-B3JdOXJ5.js";var t=e(`trailer`,{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t};
